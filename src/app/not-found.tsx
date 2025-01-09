1033c8a63839e2244984cd58cc54675866f91b43
Jansiz <ali.jansizzz@gmail.com>
2025-01-09 06:36:22 -0500

    compile fix

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,18 +5,18 @@ import { Layout } from '@/components/Layout'
 
 export default function NotFound() {
   return (
-    <Layout showFooter={false}>
+    <Layout>
       <div className="relative flex h-full items-center py-20 sm:py-36">
         <BackgroundImage className="-top-36 bottom-0" />
         <Container className="relative flex w-full flex-col items-center">
-          <p className="font-display text-2xl tracking-tight text-blue-900">
+          <p className="font-display text-4xl font-bold tracking-tight text-[#2e2e2e] sm:text-5xl">
             404
           </p>
-          <h1 className="mt-4 font-display text-4xl font-medium tracking-tighter text-blue-600 sm:text-5xl">
+          <h1 className="mt-4 font-display text-2xl font-semibold tracking-tight text-[#2e2e2e]">
             Page not found
           </h1>
-          <p className="mt-4 text-lg tracking-tight text-blue-900">
-            Sorry, we couldn’t find the page you’re looking for.
+          <p className="mt-4 text-lg tracking-tight text-[#2e2e2e]">
+            Sorry, we couldn't find the page you're looking for.
           </p>
           <Button href="/" className="mt-8">
             Go back home
